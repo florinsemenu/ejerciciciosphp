aa0e461a1e4954c423dce4a4ef17394450b23a13
Florin Semenu <florinsemenu@gmail.com>
2020-09-28 18:22:21 +0100

    ejercicio 7 hecho

---
+++ ejercicios/07.php
@@ -0,0 +1,9 @@
+<?php
+//crear array
+$jugadores = [0 => "Japón", 1 => "Francia", 2 => "Rumanía", 3 => "U.S.A."];
+//mostrar el arrary
+foreach ($jugadores as $value) {
+    echo $value . "<br>";
+}
+
+?>
