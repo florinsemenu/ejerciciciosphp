174be463f70620932fbb4f79babc31390cb5e00c
Florin Semenu <florinsemenu@gmail.com>
2020-09-28 18:14:08 +0100

    ejercicio 6 hecho

--- ejercicios/06.php
+++ ejercicios/06.php
@@ -1,9 +1,16 @@
 <?php
 
 $jugadores = [0 => "Troya", 1 => "Chris", 2 => "Jorge", 3 => "Zero", 4 => "Ruso"];
-
+echo "<ul>";
 foreach ($jugadores as $value) {
-    echo $value . "<br>";
+    echo "<li>" . $value . "</li>";
 }
+echo "</ul>";
+
+echo "<ul>";
+foreach ($jugadores as $position=>$value) {
+    echo "<li>" . $position . " : " . $value . "</li>";
+}
+echo "</ul>";
 
 ?>
