6b600af1424045179b6616aca127d24ea6f08299
Florin Semenu <florinsemenu@gmail.com>
2020-09-30 15:35:58 +0100

    ejercicio9 hecho

---
+++ ejercicios/09.php
@@ -0,0 +1,12 @@
+<?php
+//crear array
+$paises [] = "Japón";
+$paises [] = "Francia";
+$paises [] = "Rumanía";
+$paises [] = "U.S.A.";
+//mostrar el arrary
+foreach ($paises as $value) {
+    echo $value . "<br>";
+}
+
+?>
