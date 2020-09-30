45981407844ca2ccfb7d9c58624dab7dccc78214
Florin Semenu <florinsemenu@gmail.com>
2020-09-30 16:10:48 +0100

    ejercicio11 hecho

---
+++ ejercicios/11.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html><head>
+        <meta charset="UTF-8">
+</head><body>
+    <h2>Libro</h2>
+    <form method=get action="11action.php">
+        <label>Titulo</label><input type="text" value="" name="titulo"><br>
+        <label>Autor</label><input type="text" value="" name="autor"><br>
+        <label>Editorial</label><input type="text" value="" name="editorial"><br>
+        <label>Páginas</label><input type="text" value="" name="paginas"><br>
+        <input type="submit" value="enviar">
+    </form>
+</body></html>
