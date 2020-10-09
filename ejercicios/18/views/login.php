9744beb0b899647c71d8daae2709da7eca07fd06
Florin Semenu <florinsemenu@gmail.com>
2020-10-09 16:20:46 +0100

    ejercicio 18

---
+++ ejercicios/18/views/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html><head>
+        <meta charset="UTF-8">
+        <title>ejemplo</title>
+</head>
+<body>
+    <h2>Formulario</h2>
+    <form method="post" action="">
+        <label>Usuario</label><input type="text" value="" name="usuario" required> <br>
+        <label>Contraseña</label><input type="password" value="" name="contraseña" required> <br>
+        <input type="submit" value="enviar">
+    </form>
+  
+</body>
+</html>
